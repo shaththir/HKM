032a38f1af85c2ae0e2f90b17abdb9402383a58d
Razeeth734 <59931036+Razeeth734@users.noreply.github.com>
2020-01-24 11:21:56 +0530

    dog file created

---
+++ dog.java
@@ -0,0 +1,15 @@
+class dog {
+  private String name;
+  private int age;
+  private STring color;
+  
+  dog() {
+    this("browny", 12, "Brown");
+  }
+  
+  dog(String name, int age, String color) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+  
+  }
